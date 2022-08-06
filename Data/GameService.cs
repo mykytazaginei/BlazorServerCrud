8ec5c066a160eaacda3bd10df244b50c384f73bd
Nikita Zaginey <nikitazaginey1@gmail.com>
2022-08-06 19:21:23 +0200

    Implement all CRUD operations in the GameService

--- Data/GameService.cs
+++ Data/GameService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorServerCrud.Data
 {
@@ -6,14 +7,53 @@ namespace BlazorServerCrud.Data
     {
 
         private readonly DataContext _context;
+        private readonly NavigationManager _navigationManager;
 
-        public GameService(DataContext context)
+        public GameService(DataContext context, NavigationManager navigationManager)
         {
+
             _context = context;
+            _navigationManager = navigationManager;
             _context.Database.EnsureCreated();
+
         }
 
         public List<Game> Games { get; set; } = new List<Game>();
+
+        public async Task CreateGame(Game game)
+        {
+            _context.Games.Add(game);
+
+            await _context.SaveChangesAsync();
+            _navigationManager.NavigateTo("videogames");
+
+        }
+
+        public async Task DeleteGame(int id)
+        {
+
+            var dbGame = await _context.Games.FindAsync(id);
+
+            if (dbGame == null)
+                throw new Exception("No game here.");
+
+            _context.Games.Remove(dbGame);
+
+            await _context.SaveChangesAsync();
+            _navigationManager.NavigateTo("videogames");
+
+        }
+
+        public async Task<Game> GetSingleGame(int id)
+        {
+
+            var game = await _context.Games.FindAsync(id);
+
+            if (game == null)
+                throw new Exception("No game here.");
+            return game;
+
+        }
 
         public async Task LoadGames()
         {
@@ -22,5 +62,21 @@ namespace BlazorServerCrud.Data
 
         }
 
+        public async Task UpdateGame(Game game, int id)
+        {
+
+            var dbGame = await _context.Games.FindAsync(id);
+            
+            if(dbGame == null)
+                throw new Exception("No game here.");
+
+            dbGame.Name = game.Name;
+            dbGame.Developer = game.Developer;
+            dbGame.Realese = game.Realese;
+
+            await _context.SaveChangesAsync();
+            _navigationManager.NavigateTo("videogames");    
+            
+        }
     }
 }
--- Data/IGameService.cs
+++ Data/IGameService.cs
@@ -7,5 +7,13 @@
 
         Task LoadGames();
 
+        Task<Game> GetSingleGame(int id);
+
+        Task CreateGame(Game game);
+
+        Task UpdateGame(Game game, int id);
+
+        Task DeleteGame(int id);
+
     }
 }
