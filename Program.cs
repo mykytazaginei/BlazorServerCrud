eacbe531eac2260a36019aeb4fcd1860a846b721
Nikita Zaginey <nikitazaginey1@gmail.com>
2022-08-07 15:36:31 +0200

    delete default values unused by the project

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("videogamedb"));
 builder.Services.AddScoped<IGameService, GameService>();
 
