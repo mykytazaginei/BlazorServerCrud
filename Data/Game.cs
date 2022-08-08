95d2064bb0b93439c88796b8a23aa3c6afe328ec
Nikita Zaginey <nikitazaginey1@gmail.com>
2022-08-08 11:08:45 +0200

    add attributes for MinLength of fields Name and Developers

--- Data/Game.cs
+++ Data/Game.cs
@@ -6,9 +6,11 @@ namespace BlazorServerCrud.Data
     {
         public int Id { get; set; }
 
+        [StringLength(100, MinimumLength = 3)]
         [Required(ErrorMessage = "Enter a Name")]
         public string? Name { get; set; }
 
+        [StringLength(100, MinimumLength = 3)]
         [Required(ErrorMessage = "Enter a Developer")]
         public string? Developer { get; set; }
 
